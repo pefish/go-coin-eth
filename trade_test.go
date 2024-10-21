4492b8390bdac41a9d5024e34851e5a4767bd08b
pefish <darrondarron@qq.com>
2024-10-21 18:57:58 +0800

    v1.8.3

--- trade_test.go
+++ trade_test.go
@@ -11,6 +11,7 @@ func TestWallet_WETHAddressFromRouter(t *testing.T) {
 	wallet, err := NewWallet(&i_logger.DefaultLogger).InitRemote(&UrlParam{
 		RpcUrl: "https://mainnet.base.org",
 	})
+
 	go_test_.Equal(t, nil, err)
 	wethAddress, err := wallet.WETHAddressFromRouter("0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24")
 	go_test_.Equal(t, nil, err)
